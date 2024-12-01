aa60805fad7c276a3f1cf0bd660c22a3732e81e4
ᴛᴇꜱʜᴀɴᴇ ᴄʀᴀᴡꜰᴏʀᴅ <58280701+TeshaneCrawford@users.noreply.github.com>
2024-12-01 00:21:27 -0500

    add unocss ESLint configuration and update rules

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,9 @@
-// @ts-check
+import unocss from '@unocss/eslint-config/flat'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
 export default withNuxt(
   // Your custom configs here
+  unocss,
 ).overrideRules({
   'vue/max-attributes-per-line': ['warn', { singleline: 3 }],
 })
